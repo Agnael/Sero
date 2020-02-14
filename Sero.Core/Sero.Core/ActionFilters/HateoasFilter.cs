391202b4b233470e663b39bb66b90aea59f1ce00
Oleg Kuzmych <oleg.kuzmych@outlook.com>
2020-02-14 02:34:14 -0300

    Parcial roto prelaburo

--- Sero.Core/Sero.Core/ActionFilters/HateoasFilter.cs
+++ Sero.Core/Sero.Core/ActionFilters/HateoasFilter.cs
@@ -28,7 +28,7 @@ namespace Sero.Core
         }
 
         public void OnActionExecuting(ActionExecutingContext context)
-        {
+        {        
             this.ProvidedActionArguments = context.ActionArguments;
             this.CurrentAction = context.ActionDescriptor;
 
@@ -59,7 +59,7 @@ namespace Sero.Core
         public HateoasElementView GetElementView(object resource)
         {
             string resourceCode = CurrentHateoasAttr.ResourceCode;
-            var elemLinks = GetHateoasLinks(resource, resourceCode, ActionScope.Element);
+            var elemLinks = GetElementLinks(resourceCode, resource);
             var elemActions = GetHateoasActions(resource, resourceCode, ActionScope.Element);
 
             var view = new HateoasElementView(elemLinks, elemActions, resource);
@@ -71,8 +71,8 @@ namespace Sero.Core
             // Para que funcione, TODOS los GETs que se hagan de collections, deben tomar SOLO un parámetro, que debe heredar de la clase CollectionFilter
             CollectionFilter filter = collection.UsedFilter;
             string resourceCode = CurrentHateoasAttr.ResourceCode;
-            var collectionLinks = GetHateoasLinks(null, resourceCode, ActionScope.Collection);
             var collectionActions = GetHateoasActions(null, resourceCode, ActionScope.Collection);
+            var collectionLinks = GetCollectionLinks(resourceCode, filter);
 
             List<HateoasElementView> embeddedList = new List<HateoasElementView>();
             foreach (var element in collection.ElementsToReturn)
@@ -85,42 +85,174 @@ namespace Sero.Core
             return hateoasCollectionView;
         }
 
-        protected Dictionary<string, string> GetHateoasLinks(object element, string resourceCode, ActionScope scope)
+        //[NonAction]
+        //public string GetCollectionEndpointUrl(
+        //    CollectionFilter filter,
+        //    int calculatedLastPage,
+        //    bool tryPreviousPage = false,
+        //    bool tryNextPage = false,
+        //    bool tryFirst = false,
+        //    bool tryLast = false)
+        //{
+        //    var relevantParams = new Dictionary<string, string>();
+
+        //    CollectionFilter newFilter = filter.Copy();
+
+        //    if (tryPreviousPage)
+        //        newFilter.Page = filter.Page > 1 ? filter.Page - 1 : filter.Page;
+
+        //    if (tryNextPage)
+        //        newFilter.Page = filter.Page < calculatedLastPage ? filter.Page + 1 : filter.Page;
+
+        //    if (tryFirst)
+        //        newFilter.Page = 1;
+
+        //    if (tryLast)
+        //        newFilter.Page = calculatedLastPage;
+
+        //    if (!newFilter.IsDefaultTextSearch())
+        //        relevantParams.Add(nameof(newFilter.TextSearch), newFilter.TextSearch);
+
+        //    if (!newFilter.IsDefaultPage())
+        //        relevantParams.Add(nameof(newFilter.Page), newFilter.Page.ToString());
+
+        //    if (!newFilter.IsDefaultPageSize())
+        //        relevantParams.Add(nameof(newFilter.PageSize), newFilter.PageSize.ToString());
+
+        //    if (!newFilter.IsDefaultSortBy())
+        //        relevantParams.Add(nameof(newFilter.SortBy), newFilter.SortBy.ToString());
+
+        //    if (!newFilter.IsDefaultOrderBy())
+        //        relevantParams.Add(nameof(newFilter.OrderBy), newFilter.OrderBy);
+
+        //    List<string> qsParts = new List<string>();
+        //    string qs = null;
+
+        //    foreach (var param in relevantParams)
+        //        qsParts.Add(string.Format("{0}={1}", param.Key.ToLower(), param.Value));
+
+        //    if (qsParts.Count > 0)
+        //    {
+        //        string joinedParams = string.Join('&', qsParts.ToArray());
+        //        qs = string.Format("?{0}", joinedParams);
+        //    }
+
+        //    string urlWithoutQs = HttpContext.Request.Path;
+        //    string resultUrl = urlWithoutQs + qs;
+
+        //    return resultUrl;
+        //}
+
+        protected Dictionary<string, string> GetCollectionLinks(string resourceCode, CollectionFilter usedFiler)
         {
+            if (string.IsNullOrEmpty(resourceCode)) throw new ArgumentNullException(nameof(resourceCode));
+            if (usedFiler == null) throw new ArgumentNullException(nameof(usedFiler));
+
+            // FILTRA METODOS GET DEL RESOURCECODE ACTUAL
+            var relevantLinks = ActionDescriptors.Where(x => x.IsLinkFor(resourceCode, ActionScope.Collection));
+
+            // POR CADA UNO DE ESOS METODOS, CREA UN LINK. 
+            // <NO> VA A CREAR LINK SELF
             var linkMap = new Dictionary<string, string>();
-
-            foreach (ControllerActionDescriptor action in this.ActionDescriptors)
+            foreach(ActionDescriptor action in relevantLinks)
             {
-                string actionHttpMethod = action.GetHttpMethodValue();
-                bool isGetter = action.IsElementGetter();
+                if (CurrentAction.DisplayName != action.DisplayName)
+                {
+                    var httpMethodAttr = action.GetHttpMethodAttribute();
+                    string actionName = CasingUtil.UpperCamelCaseToLowerUnderscore(action.DisplayName);
+                    string href = "/" + httpMethodAttr.Template;
 
-                if (actionHttpMethod == "GET"
-                    && (isGetter || CurrentAction.DisplayName != action.ActionName))
+                    linkMap.Add(actionName, href);
+                }
+            }
+
+            // AGREGA SELF DE FORMA CUSTOM PORQUE PUEDE HABER PARAMETROS INNECESARIOS EN EL FILTRO ACTUAL QUE MANDÓ EL USER
+
+            // AGREGA LINKS DE PAGINACION
+
+
+
+            return linkMap;
+        }
+
+        protected Dictionary<string, string> GetElementLinks(string resourceCode, object element)
+        {
+            if (string.IsNullOrEmpty(resourceCode)) throw new ArgumentNullException(nameof(resourceCode));
+            if (element == null) throw new ArgumentNullException(nameof(element));
+
+            var linkMap = new Dictionary<string, string>();
+            var relevantActions = ActionDescriptors.Where(x => x.IsLinkFor(resourceCode, ActionScope.Element));
+
+            foreach(ActionDescriptor action in relevantActions)
+            {
+
+            }
+
+
+
+
+
+            foreach (ActionDescriptor action in httpGetActions)
+            {
+                bool isMainGetter = action.IsElementGetter();
+
+                if (isMainGetter || CurrentAction.DisplayName != action.DisplayName)
                 {
                     var hateoasAttr = action.GetHateoasAttribute();
 
                     if (hateoasAttr.ResourceCode == resourceCode
-                        && hateoasAttr.ActionScope == scope)
+                        && hateoasAttr.ActionScope == ActionScope.Element)
                     {
                         var httpMethodAttr = action.GetHttpMethodAttribute();
+                        string actionName = CasingUtil.UpperCamelCaseToLowerUnderscore(action.DisplayName);
 
-                        string actionName = CasingUtil.UpperCamelCaseToLowerUnderscore(action.ActionName);
-
-                        if (action.IsElementGetter())
+                        if (isMainGetter)
                             actionName = "self";
 
-                        string href = "/" + httpMethodAttr.Template;
-
-                        if (element != null)
-                            href = ReplaceUrlTemplate(href, element);
-
+                        string href = "/" + ReplaceUrlTemplate(httpMethodAttr.Template, element);
                         linkMap.Add(actionName, href);
                     }
                 }
             }
-
             return linkMap;
         }
+
+        //protected Dictionary<string, string> GetHateoasLinks(object element, string resourceCode, ActionScope scope)
+        //{
+        //    var linkMap = new Dictionary<string, string>();
+
+        //    foreach (ControllerActionDescriptor action in this.ActionDescriptors)
+        //    {
+        //        string actionHttpMethod = action.GetHttpMethodValue();
+        //        bool isGetter = action.IsElementGetter();
+
+        //        if (actionHttpMethod == "GET"
+        //            && (isGetter || CurrentAction.DisplayName != action.ActionName))
+        //        {
+        //            var hateoasAttr = action.GetHateoasAttribute();
+
+        //            if (hateoasAttr.ResourceCode == resourceCode
+        //                && hateoasAttr.ActionScope == ActionScope.Element)
+        //            {
+        //                var httpMethodAttr = action.GetHttpMethodAttribute();
+
+        //                string actionName = CasingUtil.UpperCamelCaseToLowerUnderscore(action.ActionName);
+
+        //                if (action.IsElementGetter() || CurrentAction.DisplayName == action.DisplayName)
+        //                    actionName = "self";
+
+        //                string href = "/" + httpMethodAttr.Template;
+
+        //                if (element != null)
+        //                    href = ReplaceUrlTemplate(href, element);
+
+        //                linkMap.Add(actionName, href);
+        //            }
+        //        }
+        //    }
+
+        //    return linkMap;
+        //}
 
         protected Dictionary<string, HateoasAction> GetHateoasActions(object element, string resourceCode, ActionScope scope)
         {
---
+++ Sero.Core/Sero.Core/Extensions/ActionDescriptorExtensions.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace Sero.Core
+{
+    public static class ActionDescriptorExtensions
+    {
+        public static string GetHttpMethodValue(this ActionDescriptor action)
+        {
+            string actionHttpMethod =
+                action
+                .EndpointMetadata
+                .OfType<HttpMethodAttribute>()
+                .SelectMany(x => x.HttpMethods)
+                .Distinct()
+                .First();
+
+            return actionHttpMethod;
+        }
+
+        public static bool IsHttpGet(this ActionDescriptor action)
+        {
+            string actionHttpMethod = action.GetHttpMethodValue();
+            return actionHttpMethod == HttpMethod.Get.Method;
+        }
+
+        public static HttpMethodAttribute GetHttpMethodAttribute(this ActionDescriptor action)
+        {
+            HttpMethodAttribute attr = 
+                action
+                .EndpointMetadata
+                .OfType<HttpMethodAttribute>()
+                .First();
+
+            return attr;
+        }
+
+        public static HateoasActionAttribute GetHateoasAttribute(this ActionDescriptor action)
+        {
+            var hateoasAttr = (HateoasActionAttribute)action.EndpointMetadata.FirstOrDefault(x => x is HateoasActionAttribute);
+            return hateoasAttr;
+        }
+
+        public static bool IsLinkFor(this ActionDescriptor action, string resourceCode, ActionScope scope)
+        {
+            bool isHttpGet = action.IsHttpGet();
+
+            if (!isHttpGet)
+                return false;
+
+            var hateoasAttr = action.GetHateoasAttribute();
+
+            return hateoasAttr.ResourceCode == resourceCode
+                    && hateoasAttr.ActionScope == scope;
+        }
+
+        public static bool IsActionFor(this ActionDescriptor action, string resourceCode, ActionScope scope)
+        {
+            bool isHttpGet = action.IsHttpGet();
+
+            if (isHttpGet)
+                return false;
+
+            var hateoasAttr = action.GetHateoasAttribute();
+
+            return hateoasAttr.ResourceCode == resourceCode
+                    && hateoasAttr.ActionScope == scope;
+        }
+
+        public static bool IsOfResource(this ActionDescriptor action, string resourceCode, ActionScope scope)
+        {
+            var hateoasAttr = action.GetHateoasAttribute();
+            
+            return hateoasAttr.ResourceCode == resourceCode
+                    && hateoasAttr.ActionScope == scope;
+        }
+
+        public static bool IsElementGetter(this ActionDescriptor action)
+        {
+            bool isGetter = action.EndpointMetadata.Any(x => x is ElementGetterAttribute);
+            return isGetter;
+        }
+    }
+}
